39f6d5016aeb14ada7c73dc7111809ce7ec38acc
malcolmbowers <malcolm.bowers@icloud.com>
2024-10-22 08:32:09 -0500

    Initial spring template

---
+++ src/main/java/swf/army/mil/FoodYFitness/FoodYFitnessApplication.java
@@ -0,0 +1,13 @@
+package swf.army.mil.FoodYFitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodYFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodYFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swf/army/mil/FoodYFitness/FoodYFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.FoodYFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodYFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
