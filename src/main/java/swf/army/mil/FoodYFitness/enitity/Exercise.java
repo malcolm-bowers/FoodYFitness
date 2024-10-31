c87825722bfa14ed55eb991e656708aaa7602f93
malcolmbowers <malcolm.bowers@icloud.com>
2024-10-31 12:58:55 -0500

    created the database and entity for exercise backend.

---
+++ src/main/java/swf/army/mil/FoodYFitness/enitity/Exercise.java
@@ -0,0 +1,60 @@
+package swf.army.mil.FoodYFitness.enitity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String type;
+
+    private int calories;
+
+    public Exercise() {
+    }
+
+    public Exercise(String name, String type, int calories) {
+        this.name = name;
+        this.type = type;
+        this.calories = calories;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+}
